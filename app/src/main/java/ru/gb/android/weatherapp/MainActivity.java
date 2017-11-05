0c7fd81fd79791f65a0840badc8b4d5a5bf74d03
vgonikhin <vgonikhin@gmail.com>
2017-11-06 02:42:26 +0300

    HW3

--- app/src/main/java/ru/gb/android/weatherapp/MainActivity.java
+++ app/src/main/java/ru/gb/android/weatherapp/MainActivity.java
@@ -1,29 +1,28 @@
 package ru.gb.android.weatherapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
     private Spinner spinner;
     private SharedPreferences sPref;
 
-    final String CITY_POSITION = "city_position";
+    public static final String CITY_POSITION = "city_position";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.weather_textview);
-        Button button = (Button) findViewById(R.id.show_weather_button);
+        sPref = getSharedPreferences("MyPref", MODE_PRIVATE);
+        Button button = findViewById(R.id.show_weather_button);
         button.setOnClickListener(onClickListener);
-        spinner = (Spinner) findViewById(R.id.choosecity_spinner);
+        spinner = findViewById(R.id.choosecity_spinner);
         loadCity();
     }
 
@@ -31,26 +30,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             if (view.getId() == R.id.show_weather_button) {
-                String result = Forecast.getForecast(MainActivity.this, spinner.getSelectedItemPosition());
-                textView.setText(result);
+                Intent intent = new Intent(MainActivity.this,ShowWeather.class);
+                intent.putExtra(CITY_POSITION, spinner.getSelectedItemPosition());
+                startActivity(intent);
                 saveCity();
             }
         }
     };
 
     private void saveCity() {
-        sPref = getSharedPreferences("MyPref", MODE_PRIVATE);
         SharedPreferences.Editor ed = sPref.edit();
         ed.putInt(CITY_POSITION, spinner.getSelectedItemPosition());
-        ed.commit();
+        ed.apply();
     }
 
 
     private void loadCity() {
-        sPref = getSharedPreferences("MyPref", MODE_PRIVATE);
         int position = sPref.getInt(CITY_POSITION, 0);
         spinner.setSelection(position);
-        String result = Forecast.getForecast(MainActivity.this, position);
-        textView.setText(result);
     }
 }
